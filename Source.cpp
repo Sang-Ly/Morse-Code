3a088cf8f79ae69554e5bdfdb53fb38fa558a9e2
Sang-Ly <32624248+Sang-Ly@users.noreply.github.com>
2017-11-29 14:06:24 -0600

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,42 +1,20 @@
 #include <iostream>
-#include "Morse_Code.h"
+#include "Morse_Manipulator.h"
 using namespace std;
 
-int main() {
+int main() 
+{
+	string message_1 = "_.. __.";
+	string message_2 = "ac";
 
-	string this_Should_Throw_Error_1 = "112";
-	Morse_Code throw_error1(this_Should_Throw_Error_1);		// this should throw error at position 1
+	Morse_Manipulator code;
 
-	string this_Should_Throw_Error_2 = ".._. +=/";			// this should throw error at position 6
-	Morse_Code throw_error2(this_Should_Throw_Error_2);
-
-	string encode_Message = "ac";
-	Morse_Code encode(encode_Message);		// Will print ._ _._.
-
-	string decode_Message = "_.. __.";
-	Morse_Code decode(decode_Message);		// Will print dg
-	
+	code.decode(message_1);
 	cout << endl;
-
-	/*
-	// Run this code for fun!
-
-	string message1 = ".__ .";
-	string message2 = "._ ._. .";
-	string message3 = ".._. .. _. .. ... ....";
-
-	Morse_Code code1(message1);
-	cout << " ";
-
-	Morse_Code code2(message2);
-	cout << " ";
-
-	Morse_Code code3(message3);
-	cout << " ";
+	code.encode(message_2);
 	
-	*/
-
 	cout << endl;
 	system("pause");
 	return 0;
+
 }
